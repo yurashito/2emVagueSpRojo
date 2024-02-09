692ad2ff2161c4e42d0807b3bd09019fda39fbd5
yurashito <118957930+yurashito@users.noreply.github.com>
2024-02-09 10:38:32 +0100

    mettre des tokens
    mettre des tokens  dans: mettreAnnonceFavorie
    Voir les favories

--- src/main/java/com/voiture/voiture/controller/FavorieController.java
+++ src/main/java/com/voiture/voiture/controller/FavorieController.java
@@ -1,6 +1,8 @@
 package com.voiture.voiture.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,9 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.*;
+
+import com.voiture.voiture.connex.Connexion;
+import com.voiture.voiture.modele.AnnonceAvecPhoto;
 import com.voiture.voiture.modele.Favorie;
 import com.voiture.voiture.service.FavorieService;
+import com.voiture.voiture.service.JwtTokenUtil;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+
+import java.sql.Connection;
 import lombok.AllArgsConstructor;
 
 @RestController
@@ -20,7 +30,8 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class FavorieController {
     private final FavorieService favorieService;
-
+    private final JwtTokenUtil jwtTokenUtil;
+    
     @GetMapping
     public List<Favorie> selectAll() {
         return this.favorieService.select();
@@ -35,9 +46,65 @@ public class FavorieController {
          this.favorieService.deleteFavorie(idFavorie);
     }
 
-    @PostMapping("/mettreFavorie/{idAnnonce}/{idUtilisateur}")
-    public Favorie mettreFavorie(@PathVariable int idAnnonce, @PathVariable int idUtilisateur){
-        Favorie favorie = new Favorie(idAnnonce,idUtilisateur);
-        return favorieService.mettreFavorie(favorie);
+    // @PostMapping("/mettreFavorie/{idAnnonce}/{idUtilisateur}")
+    // public Favorie mettreFavorie(@PathVariable int idAnnonce, @PathVariable int idUtilisateur){
+    //     Favorie favorie = new Favorie(idAnnonce,idUtilisateur);
+    //     return favorieService.mettreFavorie(favorie);
+    // }
+
+    @PostMapping("/mettreFavorie/{idAnnonce}")
+    public  ResponseEntity<Object> mettreFavorie( javax.servlet.http.HttpServletRequest request ,@PathVariable int idAnnonce){
+        
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String bearerToken = authorizationHeader.substring(7); // Supprimer "Bearer " du début
+                try{
+                    if(jwtTokenUtil.isTokenValid(bearerToken)){
+                      
+                        
+                        Jws<Claims> claims = jwtTokenUtil.decomposeLeToken(bearerToken);
+                        String idUtilisateur = claims.getBody().getSubject();
+                        Favorie favorie = new Favorie(idAnnonce,Integer.parseInt(idUtilisateur));
+                        Favorie resultat = new Favorie();
+                        try{
+                            resultat = (favorieService.mettreFavorie(favorie));
+                        }catch(Exception e){
+                            
+                            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                                Map.of(
+                                    "status", HttpStatus.BAD_REQUEST.value(),
+                                    "message", "Une erreur s'est produite : il y a erreur sur l'appel du fonction Favorie/mettreFavorie(favorie)",
+                                    "timestamp", System.currentTimeMillis()
+                                )
+                            );
+                        }
+                        return ResponseEntity.ok(resultat);
+                    }else{
+                        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                            Map.of(
+                                "status", HttpStatus.BAD_REQUEST.value(),
+                                "message", "Une erreur s'est produite : le token n'est plus valide",
+                                "timestamp", System.currentTimeMillis()
+                            )
+                        );
+                    } 
+                }catch(Exception e){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        Map.of(
+                            "status", HttpStatus.BAD_REQUEST.value(),
+                            "message", e,
+                            "timestamp", System.currentTimeMillis()
+                        )
+                    );
+                }
+            } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                Map.of(
+                    "status", HttpStatus.BAD_REQUEST.value(),
+                    "message", "Une erreur s'est produite : le token n'est plus valide",
+                    "timestamp", System.currentTimeMillis()
+                )
+            );
+        }
     }
 }
--- src/main/java/com/voiture/voiture/controller/V_TousAnnonceFavorieController.java
+++ src/main/java/com/voiture/voiture/controller/V_TousAnnonceFavorieController.java
@@ -1,8 +1,11 @@
 package com.voiture.voiture.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,16 +15,75 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.voiture.voiture.modele.*;
+import com.voiture.voiture.repository.AnnonceDetailRepository;
+import com.voiture.voiture.repository.AnnonceRepository;
 import com.voiture.voiture.service.*;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
 
 @RestController
 @CrossOrigin
 @RequestMapping("/V_favories")
 public class V_TousAnnonceFavorieController {
-    @PostMapping("/favorieParUtilisateur/{idUtilisateur}")
-    public List<V_TousAnnonceFavorie> listeAnnoncePicture2(@PathVariable int idUtilisateur)throws Exception{
-        V_TousAnnonceFavorie appel = new V_TousAnnonceFavorie();
-        List<V_TousAnnonceFavorie> liste = appel.HistoriqueAnnonceUtilisateur(null,idUtilisateur);
-        return liste;
+    // private final AnnonceService  annonceService;
+    // private final AnnonceDetailRepository annonceDetailRepository;
+    // private final AnnonceRepository annonceRepository;
+    // private final AnnonceDetailService annonceDetailService;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    public V_TousAnnonceFavorieController( JwtTokenUtil jwtTokenUtil ){
+        this.jwtTokenUtil= jwtTokenUtil;
+    }
+
+    // @PostMapping("/favorieParUtilisateur/{idUtilisateur}")
+    // public List<V_TousAnnonceFavorie> listeAnnoncePicture2(@PathVariable int idUtilisateur)throws Exception{
+    //     V_TousAnnonceFavorie appel = new V_TousAnnonceFavorie();
+    //     List<V_TousAnnonceFavorie> liste = appel.HistoriqueAnnonceUtilisateur(null,idUtilisateur);
+    //     return liste;
+    // }
+
+    @PostMapping("/favorieParUtilisateur")
+    public ResponseEntity<Object>  listeAnnoncePicture2(javax.servlet.http.HttpServletRequest request)throws Exception{
+     
+        String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            String bearerToken = authorizationHeader.substring(7); // Supprimer "Bearer " du début
+                try{
+                    if(jwtTokenUtil.isTokenValid(bearerToken)){
+                        Jws<Claims> claims = jwtTokenUtil.decomposeLeToken(bearerToken);
+                        System.out.println("le idUtilisateur est : "+Integer.parseInt(claims.getBody().getSubject()));
+                        String idUtilisateur = claims.getBody().getSubject();
+                        V_TousAnnonceFavorie appel = new V_TousAnnonceFavorie();
+                        List<V_TousAnnonceFavorie> liste = appel.HistoriqueAnnonceUtilisateur(null,Integer.parseInt(idUtilisateur));
+                        return ResponseEntity.ok(liste);
+                    }else{
+                        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                            Map.of(
+                                "status", HttpStatus.BAD_REQUEST.value(),
+                                "message", "Une erreur s'est produite : le token n'est plus valide",
+                                "timestamp", System.currentTimeMillis()
+                            )
+                        );
+                    } 
+                }catch(Exception e){
+                    return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                        Map.of(
+                            "status", HttpStatus.BAD_REQUEST.value(),
+                            "message", e,
+                            "timestamp", System.currentTimeMillis()
+                        )
+                    );
+                }
+            } else {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(
+                Map.of(
+                    "status", HttpStatus.BAD_REQUEST.value(),
+                    "message", "Une erreur s'est produite : le token n'est plus valide",
+                    "timestamp", System.currentTimeMillis()
+                )
+            );
+        }
     }
 }
